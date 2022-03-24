ac1b1abd6804916c723c54c9140672d488eafea4
Dmitry Chekir <chekirdmitry@gmail.com>
2022-03-24 19:56:27 +0300

    [+] Added: monsters

--- projects/dungeoncrawl/src/main.rs
+++ projects/dungeoncrawl/src/main.rs
@@ -41,9 +41,9 @@ fn main() -> BError {
 }
 
 struct State {
-    ecs : World,
-    resources : Resources,
-    systems: Schedule
+    ecs: World,
+    resources: Resources,
+    systems: Schedule,
 }
 
 impl State {
@@ -53,13 +53,18 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
 
         Self {
             ecs,
             resources,
-            systems: build_scheduler()
+            systems: build_scheduler(),
         }
     }
 }
